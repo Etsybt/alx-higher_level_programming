c82e3573cfa9473b8553261121824b260bdd760c
Etsybt <hmidouchnouhaila2@gmail.com>
2023-09-05 13:06:17 +0000

    update

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-# for i in range(97, 123):
-#print(chr(i), end="")
-output = ""
 for i in range(97, 123):
-	output += chr(i)
-print(output, end="")
+    print("{}".format(chr(i)), end="")
+
