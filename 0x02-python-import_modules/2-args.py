272f320730d4de8d71ac25eb9acb73671a8ec6f1
Etsybt <hmidouchnouhaila2@gmail.com>
2023-09-07 13:25:12 +0000

    update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    count = len(sys.argv) - 1
-    if count == 0:
-        print("0 arguments.")
-    elif count == 1:
+import sys
+
+# Remove the script name (first argument) from sys.argv
+arguments = sys.argv[1:]
+
+num_arguments = len(arguments)
+
+    if num_arguments == 0:
+	print("0 arguments.")
+    elif num_arguments == 1:
 	print("1 argument:")
     else:
-	print("{} arguments:".format(count))
-    for i in range(count):
-	print("{}: {}".format(i + 1, sys.argv[i + 1]))
+        print("{} arguments:".format(num_arguments))
+
+    for i, arg in enumerate(arguments, start=1):
+	print("{}: {}".format(i, arg))
 
 
