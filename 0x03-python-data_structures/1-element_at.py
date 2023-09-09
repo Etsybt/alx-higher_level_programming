8898fee3c2fa505fb19bd768c4359100dee892fe
Etsybt <hmidouchnouhaila2@gmail.com>
2023-09-09 15:30:00 +0000

    update

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    for idx in my_list:
-        if idx < 0 or idx > (en(my_list) - 1):
-            return None
-        return my_list[idx]
+    if idx < 0 or idx > (en(my_list) - 1):
+        return None
+    return my_list[idx]
 	    
