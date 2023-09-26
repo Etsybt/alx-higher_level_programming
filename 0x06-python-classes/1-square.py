11dd53a6b0e3f1a43c8f9579a09d23b8dd63eb8a
Etsybt <hmidouchnouhaila2@gmail.com>
2023-09-26 21:17:08 +0000

    update

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""square class"""
+"""Square class"""
 
 
 class Square:
     """
-    defines a square.
+    Defines a square.
     
     Attributes:
         size: size of square.
