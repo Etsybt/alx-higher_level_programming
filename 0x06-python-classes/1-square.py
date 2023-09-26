6ae09da16c15e3995befc81a5a883bbf213f3f73
Etsybt <hmidouchnouhaila2@gmail.com>
2023-09-26 21:33:05 +0000

    update

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-""" Square class"""
+"""Square class"""
 
 
 class Square:
-    """Defines a square by its size
+    """
+    Defines a square by its size.
 	    
     Attributes:
-        size: square size
+        size: square size.
     """
     def __init__(self, size):
-        """Initialising the data.
+        """
+        Initialising the data.
+
         Args:
             size: size of square.
         """
