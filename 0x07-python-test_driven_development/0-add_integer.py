46e67298be4da9c1b471220c4304cf16aff611d4
Etsybt <hmidouchnouhaila2@gmail.com>
2023-10-01 19:59:05 +0000

    update

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -17,12 +17,13 @@ def add_integer(a, b=98):
         TypeError: if a or b isn't an integer or a float.
     """
 
-    if not isinstance(a, (int, float)):
+    if type(a) not in (int, float):
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
+    elif type(b) not in (int, float):
         raise TypeError("b must be an integer")
-    
-    a = int(a)
-    b = int(b)
+    else:
+        return int(a) + int(b)
 
-    return a + b
+    if __name__ == "__main__":
+        import doctest
+        doctest.testfile("tests/0-add_integer.txt")
