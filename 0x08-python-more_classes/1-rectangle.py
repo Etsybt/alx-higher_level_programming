e4bd1189bfce618b05d532be18910d75d0e0ee0f
Etsybt <hmidouchnouhaila2@gmail.com>
2023-10-02 12:50:46 +0000

    update

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""rectangle class"""
+
+
+class Rectangle:
+    """Defiines a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """
+        Initialising the date.
+
+        Args:
+            width: the rectangle's width.
+            height: the rectangle's height.
+        """
+
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """returns the rectangle's width"""
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        property setter for width.
+
+        Args:
+            value: width of the rectangle.
+
+        Raise:
+            TypeError: width must be an integer.
+            ValueError: width must be >= 0.
+        """
+
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """returns the rectangle's height"""
+
+        return self.__height
+  
+    @height.setter
+    def height(self, value):
+        """
+        Property setter for height.
+
+        Args:
+            value: the rectangle's height.
+
+        Raise:
+            TypeError: height must be an integer.
+            ValueError: height must be >= 0.
+        """
+
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
