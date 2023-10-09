0e9e6318a05c0e16c7e1e7617a4213fe407bc7e5
Etsybt <hmidouchnouhaila2@gmail.com>
2023-10-09 15:12:26 +0000

    update

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-Rectangle = __import__('9-rectangle').Rectangle
 """
 Defines a Square class based on the parentClass Rectangle.
 """
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
