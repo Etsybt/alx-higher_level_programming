66a9bb933031b2c14cf0a82c4ed27f660cba8544
Etsybt <hmidouchnouhaila2@gmail.com>
2023-10-09 15:38:32 +0000

    update

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Defines a Square class."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Defines a square class inherited from the class Rectangle."""
+	    
+    def __init__(self, size):
+        """Defines the square by its size.
+		
+        Args:
+            size (int): the square's size.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+			
+    def area(self):
+        """Defines the square's area.
+		
+        Returns:
+            int: the area of the square.
+        """
+        return self.__size ** 2
+
+    def __str(self):
+        """returns a statement"""
+
+        return"[Square] {}/{}".format(self.__size, self.__size)
