f9caaa7fe1ebf343f95c2081e643eaa541ff8acb
Etsybt <hmidouchnouhaila2@gmail.com>
2023-10-09 12:53:07 +0000

    update

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""BaseGeometry class"""
+
+
+class BaseGeometry:
+    """BaseGeometry class defined"""
+
+    @classmethod
+    def area(self):
+        """defines the area"""
+
+        raise Exception("area() is not implemented")
