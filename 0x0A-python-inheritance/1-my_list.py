cba3b34ab6912f15497cb3d5a60a705925dd4ba8
Etsybt <hmidouchnouhaila2@gmail.com>
2023-10-09 12:32:51 +0000

    update

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,9 +6,9 @@ Mylist class
 
 class MyList(list):
     """defines the class"""
-    pass
 
     def print_sorted(self):
         """prints the list, but sorted (ascending sort)"""
 
-        print(sorted(list(self)))
+        sorted_list = sorted(self)
+        print(sorted_list)
