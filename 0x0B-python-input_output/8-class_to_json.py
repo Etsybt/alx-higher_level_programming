9c3139d541125008d190a09c0f81670d97cac8d3
Etsybt <hmidouchnouhaila2@gmail.com>
2023-10-10 13:22:30 +0000

    update

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -3,9 +3,10 @@
 
 
 def class_to_json(obj):
-    """returns the dictionary description with simple data structure 
-    (list, dictionary, string, integer and boolean)
-    for JSON serialization of an object
+    """returns the dictionary description with simple data structure.
+
+    Args:
+        obj: an instance of a Class
     """
     if not isinstance(obj, type):
         return (obj.__dict__)
