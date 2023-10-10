dd4edc2d306c203563f62b77e819e9b5749b0228
Etsybt <hmidouchnouhaila2@gmail.com>
2023-10-10 14:03:29 +0000

    update

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Defines a class Student."""
+
+
+class Student:
+    """Represent a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Get a dictionary representation of the Student.
+        
+        Args:
+            attrs (list): the attr to be represented.
+        """
+        if  (isinstance(attrs, list) and 
+			all(isinstance(x, str) for x in attrs)):
+            return {n: getattr(self, n) for n in attrs if hasattr(self, n)}
+        return self.__dict__
