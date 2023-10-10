f264262f1d920a9be6af06c55e68bf4b73b130a6
Etsybt <hmidouchnouhaila2@gmail.com>
2023-10-10 13:57:05 +0000

    update

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-"""Defines a Student class."""
+"""Defines a class Student."""
 
 
 class Student:
-    """Represent a Student class."""
-	    
+    """Represent a student."""
+
     def __init__(self, first_name, last_name, age):
-        """Initialize a Student object.
-		
+        """Initialize a new Student.
+
         Args:
-            first_name (str): student's first_name.
-            last_name (str): student's last_name.
-            age (int): student's age.
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-	
+
     def to_json(self):
-        """Return a dictionary representation of the Student."""
+        """Get a dictionary representation of the Student."""
         return self.__dict__
