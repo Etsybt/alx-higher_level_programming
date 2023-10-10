04a8c902fb9ad4b6001d3253ba9c3d3166ebf2d0
Etsybt <hmidouchnouhaila2@gmail.com>
2023-10-10 09:29:26 +0000

    update

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-"""defines a read_file function"""
 
 
 def read_file(filename=""):
-    """reads a file"""
     with open(filename, "r", encoding="utf-8") as file:
-        print(file.read())
+        print(file.read(), end="")
