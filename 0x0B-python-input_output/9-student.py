67270234167a891f70a034828e1d84653d955d2a
Etsybt <hmidouchnouhaila2@gmail.com>
2023-10-10 10:34:00 +0000

    update

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""defines Student class"""
+
+
+class Student:
+    """defines a student class"""
+    
+    def __init__(self, first_name, last_name, age):
+        """public instance attributes.
+
+        Args:
+            first_name: student's first_name.
+            last_name: student's last_name.
+            age: student's age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        retrieves a dictionary representation of a Student instance
+        """
+        return self.__dict__.copy()
