055ab2ff9818cd69c19adc33b5ed7d4d1f10ca9d
Etsybt <hmidouchnouhaila2@gmail.com>
2023-10-10 10:21:56 +0000

    update

--- 0x0B-python-input_output/8-main.py
+++ 0x0B-python-input_output/8-main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-Myclss = __import__('8-my_class').MyClass
+MyClass = __import__('8-my_class').MyClass
 class_to_json = __import__('8-class_to_json').class_to_json
 
 m = MyClass("John")
