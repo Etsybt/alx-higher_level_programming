c4d3517d42c3750c7a7be6d48436dc6242510cbf
Etsybt <hmidouchnouhaila2@gmail.com>
2023-10-10 14:17:35 +0000

    update

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -11,11 +11,11 @@ def pascal_triangle(n):
 	
     triangle = [[1]]
     
-    while len(triangle) < n:
+    while len(triangle) != n:
         last_row = triangle[-1]
         new_row = [1]
-        for i in range(1, len(last_row)):
-            new_row.append(last_row[i - 1] + last_row[i])
+        for i in range(len(last_row) - 1):
+            new_row.append(last_row[i] + last_row[i + 1])
         new_row.append(1)
         triangle.append(new_row)
 	    
