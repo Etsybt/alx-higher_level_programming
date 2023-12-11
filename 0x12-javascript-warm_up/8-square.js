7bcaf446c7057d6804bbed559a7a44b40acb5ecf
Etsybt <hmidouchnouhaila2@gmail.com>
2023-12-11 17:19:24 +0000

    update

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,16 +4,15 @@ const arg = process.argv[2];
 
 if (!isNaN(arg)) {
   const size = parseInt(arg);
-  if (size > 0) { 
-	  for (let i = 0; i < size; i++) {
-		  let row = '';
-		  for (let j = 0; j < size; j++) {
-			  row += 'X';
-		  }
-		  console.log(row);
-	  }
+  if (size > 0) {
+    for (let i = 0; i < size; i++) {
+      let row = '';
+      for (let j = 0; j < size; j++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
   }
-}
-else {
-	  console.log('Missing size');
+} else {
+  console.log('Missing size');
 }
---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+function add (a, b) {
+  return a + b;
+}
+
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+
+if (!isNaN(a) && !isNaN(b)) {
+  const result = add(a, b);
+  console.log(result);
+} else {
+  console.log('NaN');
+}
