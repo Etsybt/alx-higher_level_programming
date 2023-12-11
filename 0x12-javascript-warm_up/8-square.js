75a4915e9726de696ba6d5c8a2221a252ce8f857
Etsybt <hmidouchnouhaila2@gmail.com>
2023-12-11 17:10:28 +0000

    update

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const arg = process.argv[2];
+
+if (!isNaN(arg)) {
+  const size = parseInt(arg);
+  if (size > 0) { 
+	  for (let i = 0; i < size; i++) {
+		  let row = '';
+		  for (let j = 0; j < size; j++) {
+			  row += 'X';
+		  }
+		  console.log(row);
+	  }
+  }
+}
+else {
+	  console.log('Missing size');
+}
