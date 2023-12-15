1720dc1c38cdc3f076f7138813d9289c48a3d958
Etsybt <hmidouchnouhaila2@gmail.com>
2023-12-15 17:24:52 +0000

    update

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
+import sys
+from sys import argv
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE BINARY name = %s", [argv[4]])
+
+    n = cursor.fetchall()
+    for x in n:
+        print(x)
+    cursor.close()
+    db.close()
